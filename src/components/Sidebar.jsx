52ef7c27acc6234859c1ff1a74342c6218c0eaf6
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-03-28 11:51:06 +0400

    add nested routes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from 'react-router-dom'
+
 import styles from './Sidebar.module.css'
 import Logo from './Logo'
 import AppNav from './AppNav'
@@ -8,7 +10,7 @@ export default function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
 
       <footer className={styles.footer}>
         <p className={styles.copyright}>
